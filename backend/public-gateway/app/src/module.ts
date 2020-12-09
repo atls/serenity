181cebcc1a4af37dc9ab2c88236127f7fa5af9da
unmyke <unmyke@gmail.com>
2020-12-09 14:40:14 +0800

    fixup! feat(public-gateway): publish collaboration service

--- backend/public-gateway/app/src/module.ts
+++ backend/public-gateway/app/src/module.ts
@@ -4,6 +4,7 @@ import { APP_INTERCEPTOR }       from '@nestjs/core'
 import { GraphQLModule }         from '@nestjs/graphql'
 
 import { CatalogModule }         from '@public-gateway/catalog'
+import { CollaborationModule }   from '@public-gateway/collaboration'
 import { FilesModule }           from '@public-gateway/files'
 import { IdentityModule }        from '@public-gateway/identity'
 
@@ -32,6 +33,7 @@ const playground =
     IdentityModule,
     CatalogModule,
     FilesModule,
+    CollaborationModule,
   ],
   providers: [
     {
