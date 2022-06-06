23e5fab6dee975d5a25e32a1688c044630ca28df
Kirill Zolottsev <carmine@atls.team>
2022-06-06 20:57:34 +0400

    feat: update icons

--- ui/icons/src/ArrowLeftIcon.tsx
+++ ui/icons/src/ArrowLeftIcon.tsx
@@ -1,17 +1,25 @@
-import React from 'react'
-export const ArrowLeftIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    xmlns='http://www.w3.org/2000/svg'
-    width='1em'
-    height='1em'
-    viewBox='0 0 24 24'
-    fill='none'
-    stroke="{props.color || '#FFFFFF'} // eslint-disable react/destructuring-assignment"
-    strokeWidth={2}
-    strokeLinecap='butt'
-    strokeLinejoin='bevel'
-    {...props}
-  >
-    <path d='M15 18l-6-6 6-6' />
-  </svg>
-)
+import { useTheme }  from '@emotion/react'
+
+/* eslint-disable */
+import React         from 'react'
+
+import { IconProps } from '../icons.interfaces'
+export const ArrowLeftIcon = (props: IconProps) => {
+  const theme: any = useTheme()
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='1em'
+      height='1em'
+      viewBox='0 0 24 24'
+      fill='none'
+      stroke={theme.colors[props.color] || props.color || '#000000'}
+      strokeWidth={2}
+      strokeLinecap='butt'
+      strokeLinejoin='bevel'
+      {...props}
+    >
+      <path d='M15 18l-6-6 6-6' />
+    </svg>
+  )
+}
--- ui/icons/src/PlusIcon.tsx
+++ ui/icons/src/PlusIcon.tsx
@@ -1,13 +1,21 @@
-import React from 'react'
-export const PlusIcon = (props: React.SVGProps<SVGSVGElement>) => (
-  <svg
-    width='1em'
-    height='1em'
-    viewBox='0 0 24 24'
-    fill='none'
-    xmlns='http://www.w3.org/2000/svg'
-    {...props}
-  >
-    <path d='M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z' fill='#999999' />
-  </svg>
-)
+import { useTheme }  from '@emotion/react'
+
+/* eslint-disable */
+import React         from 'react'
+
+import { IconProps } from '../icons.interfaces'
+export const PlusIcon = (props: IconProps) => {
+  const theme: any = useTheme()
+  return (
+    <svg
+      width='1em'
+      height='1em'
+      viewBox='0 0 24 24'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
+      <path d='M19 13H13V19H11V13H5V11H11V5H13V11H19V13Z' fill='#999999' />
+    </svg>
+  )
+}
