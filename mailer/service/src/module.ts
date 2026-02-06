ee0dfd0b6884eafab30f76399c591798aff2463e
vwency <veniaminshp@gmail.com>
2026-02-06 13:58:59 +0600

    chore: rollback module

--- mailer/service/src/module.ts
+++ mailer/service/src/module.ts
@@ -1,15 +1,20 @@
-import { Module } from '@nestjs/common'
-import { BusSharedModule } from './shared/bus-shared.module'
-import { ApiModule } from '@mailer/api'
-import { DbModule } from '@mailer/db'
-import { RendererModule } from '@mailer/renderer'
-import { TransportModule } from '@mailer/transport'
+import { BusModule }                from '@monstrs/nestjs-bus'
+import { Module }                   from '@nestjs/common'
+
+import { ApiModule }                from '@mailer/api'
+import { DbModule }                 from '@mailer/db'
+import { RendererModule }           from '@mailer/renderer'
+import { TransportModule }          from '@mailer/transport'
+
 import { EmailVerificationHandler } from './handlers'
-import { ResetPasswordHandler } from './handlers'
+import { ResetPasswordHandler }     from './handlers'
 
 @Module({
   imports: [
-    BusSharedModule,
+    BusModule.forRabbitMq({
+      queueName: 'mailer',
+      connectionString: process.env.BUS_URL || 'amqp://local:password@rabbitmq:5672/?heartbeat=30',
+    }),
     DbModule,
     ApiModule,
     RendererModule,
