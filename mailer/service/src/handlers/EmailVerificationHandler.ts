154e10347a54d97e459cfe72db91bde01be9a46b
Andrew Ghostuhin <me@torinasakura.name>
2026-06-12 19:10:10 +0300

    refactor(mailer): consume identity events through nest rmq

--- mailer/service/src/handlers/EmailVerificationHandler.ts
+++ mailer/service/src/handlers/EmailVerificationHandler.ts
@@ -1,15 +1,15 @@
+import { Controller }       from '@nestjs/common'
+import { EventPattern }     from '@nestjs/microservices'
+import { InjectRepository } from '@nestjs/typeorm'
 import { Repository }       from 'typeorm'
 
 import { UserRegistered }   from '@identity/domain/src/events/UserRegistered'
 import { Sending }          from '@mailer/db'
 import { Renderer }         from '@mailer/renderer'
 import { Transport }        from '@mailer/transport'
-import { HandlesMessage }   from '@serenity/nestjs-bus'
-import { InjectRepository } from '@nestjs/typeorm'
-import { Handler }          from '@node-ts/bus-core'
 
-@HandlesMessage(UserRegistered)
-export class EmailVerificationHandler implements Handler<UserRegistered> {
+@Controller()
+export class EmailVerificationHandler {
   constructor(
     @InjectRepository(Sending)
     private readonly sendingRepository: Repository<Sending>,
@@ -17,6 +17,7 @@ export class EmailVerificationHandler implements Handler<UserRegistered> {
     private renderer: Renderer
   ) {}
 
+  @EventPattern(UserRegistered.NAME)
   async handle({ email }: UserRegistered): Promise<void> {
     const sending = await this.sendingRepository.save(
       this.sendingRepository.create({
--- mailer/service/src/module.ts
+++ mailer/service/src/module.ts
@@ -4,23 +4,12 @@ import { ApiModule }                from '@mailer/api'
 import { DbModule }                 from '@mailer/db'
 import { RendererModule }           from '@mailer/renderer'
 import { TransportModule }          from '@mailer/transport'
-import { BusModule }                from '@serenity/nestjs-bus'
 
 import { EmailVerificationHandler } from './handlers/index.js'
 import { ResetPasswordHandler }     from './handlers/index.js'
 
 @Module({
-  imports: [
-    BusModule.forRabbitMq({
-      queueName: 'mailer',
-      connectionString:
-        process.env.BUS_URL || 'amqp://local:password@rabbitmq:5672/?heartbeat=30&frameMax=8192',
-    }),
-    DbModule,
-    ApiModule,
-    RendererModule,
-    TransportModule,
-  ],
-  providers: [EmailVerificationHandler, ResetPasswordHandler],
+  imports: [DbModule, ApiModule, RendererModule, TransportModule],
+  controllers: [EmailVerificationHandler, ResetPasswordHandler],
 })
 export class AppModule {}
